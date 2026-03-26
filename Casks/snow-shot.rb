171921adb3db929cec0fc6e981e71426051ae894
YU-3 <2747046473@qq.com>
2026-03-26 15:17:22 +0800

    sha256

--- Casks/snow-shot.rb
+++ Casks/snow-shot.rb
@@ -3,8 +3,8 @@ cask "snow-shot" do
       intel: "x64"
 
   version "0.7.8-beta"
-  sha256 arm:   "placeholder",
-         intel: "placeholder"
+  sha256 arm:   "ea041b903e7d2c0a2520f7fe01ee52a4628c7e1edb0bdcb828c036d78c5dc341",
+         intel: "5d37c525ee88e541e6e8ad7d29326b7e460b62a02b8f7ca01c81ce1c44f48036"
 
   url "https://github.com/mg-chao/snow-shot/releases/download/v#{version}/Snow.Shot_#{version}_#{arch}_darwin.dmg"
   name "Snow Shot"
